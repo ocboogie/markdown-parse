ce6dcbf6156474a0f980316be42c1bcd66fd0d52
ocboogie <boogie@mikulec.com>
2022-01-20 11:57:34 -0800

    Reject extra space between ](

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,7 +18,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (nextCloseBracket + 1 == openParen) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;

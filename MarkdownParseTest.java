009720967872f1624f613fc13dc5e3475bea758e
ocboogie <boogie@mikulec.com>
2022-01-27 11:38:04 -0800

    Add JUnit and a couple of example tests

---
+++ MarkdownParseTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(3, 1 + 1);
+    }
+
+    @Test
+    public void addition2() {
+        assertEquals(2, 1 + 1);
+    }
+}
